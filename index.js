f8596a7ece0fb11eab14e66d6d90b29a2aad28af
Ahmar khan <ahmarbilal223344@gmail.com>
2022-12-30 22:39:03 +0500

    stuck

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const inp = document.getElementById("inp");
 const list = document.getElementById("list");
 const add = document.getElementById("add");
+const del = document.querySelectorAll(".del");
+console.log(del);
+
+function addingLi() {
+  if (inp.value != "") {
+    let li = `<li>${inp.value}<i class="del fa-solid fa-trash"></i></li>`;
+
+    list.innerHTML += li;
+  }
+}
 
 add.addEventListener("click", (e) => {
-  let li = `<li>${inp.value}<i class="fa-solid fa-trash"></i></li>`;
-  list.innerHTML += li;
+  addingLi();
+});
+
+document.addEventListener("click", (e) => {
+  console.log(e.target);
 });
