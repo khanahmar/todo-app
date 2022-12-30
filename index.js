eb91b46bdc0c1451f2130429970fb7fafcc02c9f
Ahmar khan <ahmarbilal223344@gmail.com>
2022-12-30 22:05:45 +0500

    done

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const inp = document.getElementById("inp");
 const list = document.getElementById("list");
 const add = document.getElementById("add");
 
-add.
+add.addEventListener("click", (e) => {
+  let li = `<li>${inp.value}<i class="fa-solid fa-trash"></i></li>`;
+  list.innerHTML += li;
+});
